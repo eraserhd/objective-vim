4ba2c4eb0395a73bfdeea6cdc47367074b625fcd
Jason Felice <jason.m.felice@gmail.com>
2012-09-10 16:27:10 -0400

    Convert bin/build-ruby to functions

--- functions.sh
+++ functions.sh
@@ -2,8 +2,25 @@
 objvim_prefix="/opt/objvim"
 objvim_compiledby="Jason Felice <jason.m.felice@gmail.com>"
 
+function set_up_environment() {
+	export CFLAGS="-I${objvim_prefix}/include"
+	export LDFLAGS="-L${objvim_prefix}/lib"
+}
+
 function unpack() {
 	rm -rf "$1"*
 	tar xzf src/"$1"*.tar.gz
 	cd "$1"*
 }
+
+function build_ruby() {
+	unpack ruby
+	set_up_environment
+	./configure \
+		--program-prefix=objvim_ \
+		--prefix=${objvim_prefix} \
+		--enable-shared
+	make
+	make install
+}
+
